aaea7976eec69cb614d2229a9bc481ee92668afe
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-08-12 19:24:37 +0530

    Added a new background

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from tkinter.ttk import *
 from time import strftime
 root = Tk()
 root.title('Digital Clock')
+root.config(bg='black')
 
 def update_time():
     main_text.config(text=(strftime('%H:%M:%S %p')))
     main_text.after(1000, update_time)
 
-main_text = Label(root, font=('consolas', 40, 'bold'), foreground='cyan')
+main_text = Label(root, font=('consolas', 40, 'bold'), foreground='cyan', background='black')
 main_text.pack(anchor=CENTER, padx=20, pady=20)
 
 update_time()
